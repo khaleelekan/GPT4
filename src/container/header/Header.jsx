d142fe3c6e54137cc07ae2c135bd30efb769261d
unknown <kalilulekan@gmail.com>
2023-01-18 23:54:46 +0100

    header text

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import './header.css'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>let's build something amazing with GPT-3 OpenAI</h1>
+      </div>
+    </div>
   )
 }
 
