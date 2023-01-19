d345e6a147e625c3d78fc4ced63490bdd14e7aed
unknown <kalilulekan@gmail.com>
2023-01-19 17:21:36 +0100

    changes

--- src/container/header/Header.jsx
+++ src/container/header/Header.jsx
@@ -10,7 +10,6 @@ const Header = () => {
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. 
           Indulgence way everything joy alteration boisterous the attachment.
            Party we years to order allow asked of.</p>
-
         <div className='gpt3__header-content__input'>
           <input type='email' placeholder='your email address'></input>
           <button type='button'>Get Started</button>
@@ -20,7 +19,6 @@ const Header = () => {
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
     </div>
-
     <div className="gpt3__header-image">
       <img src={ai} alt='ai'/>
     </div>
